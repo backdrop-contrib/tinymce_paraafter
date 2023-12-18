01e3fa7f0207b79ad9737d9771879d437d72fd50
indigoxela <user@olga.indigofloat.at>
2023-12-18 13:49:22 +0100

    Issue #1: Switch TinyMCE plugin to IIFE

--- js/plugins/paraafter/plugin.js
+++ js/plugins/paraafter/plugin.js
@@ -2,38 +2,40 @@
  * @file
  * TinyMCE paragraph after plugin.
  */
-"use strict";
+(function () {
 
-tinymce.PluginManager.add('paraafter', function (editor, url) {
-  editor.ui.registry.addButton('paraafter', {
-    icon: 'paraafter',
-    tooltip: editor.options.get('paraafterTooltip'),
-    onAction: function (api) {
-      paraafterTools.insert(editor);
+  'use strict';
+
+  const paraafterInsert = function (editor) {
+    let node = editor.selection.getNode();
+    let parent = node;
+    while (node = node.parentNode) {
+      if (node.nodeName == 'BODY') {
+        break;
+      }
+      parent = node;
     }
-  });
-
-  editor.ui.registry.addMenuItem('paraafter', {
-    icon: 'paraafter',
-    text: editor.options.get('paraafterTooltip'),
-    onAction: function (api) {
-      paraafterTools.insert(editor);
-    }
-  });
-});
-
-const paraafterTools = {}
-
-paraafterTools.insert = function (editor) {
-  let node = editor.selection.getNode();
-  let parent = node;
-  while (node = node.parentNode) {
-    if (node.nodeName == 'BODY') {
-      break;
-    }
-    parent = node;
+    let p = editor.dom.create('p', {}, '&nbsp;');
+    editor.dom.insertAfter(p, parent);
+    editor.selection.setCursorLocation(p, 0);
   }
-  let p = editor.dom.create('p', {}, '&nbsp;');
-  editor.dom.insertAfter(p, parent);
-  editor.selection.setCursorLocation(p, 0);
-}
+
+  tinymce.PluginManager.add('paraafter', function (editor, url) {
+    editor.ui.registry.addButton('paraafter', {
+      icon: 'paraafter',
+      tooltip: editor.options.get('paraafterTooltip'),
+      onAction: function (api) {
+        paraafterInsert(editor);
+      }
+    });
+
+    editor.ui.registry.addMenuItem('paraafter', {
+      icon: 'paraafter',
+      text: editor.options.get('paraafterTooltip'),
+      onAction: function (api) {
+        paraafterInsert(editor);
+      }
+    });
+  });
+
+})();
